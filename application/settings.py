ba380f9d465d26804f22806e8c50a25d8c7f368e
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-29 20:09:50 +0700

    Feat: add profile picture url/directory

--- application/settings.py
+++ application/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'user-login'
