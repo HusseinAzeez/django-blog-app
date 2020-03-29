c95475d5cf8112b625eed4288a51d4206692e35e
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-29 14:09:21 +0700

    Feat: use post model to pass data to view

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Local imports
+from blog.models import Post
 
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Homepage'
     }
     return render(request, 'home.html', context)
