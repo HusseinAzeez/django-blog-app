b93b0f3c937a49ced6b79ff67ef85459c72c3e83
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-29 13:04:36 +0700

    Create blog app

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1>Blog App Homepage</h1>")
+    context = {
+        'posts': posts,
+        'title': 'Homepage'
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog App About page</h1>")
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'about.html', context)
