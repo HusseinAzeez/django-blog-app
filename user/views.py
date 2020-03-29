7c7b9149ed7a28f31444bc86eca6d420069b99e5
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-29 20:46:52 +0700

    Feat: Add update profile feature

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required 
 from django.contrib import messages
 # Local imports
-from user.forms import UserRegisterForm
+from user.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -25,4 +25,25 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == 'POST':
+        # Instance: is the instance of the expected object
+        # request contains the current user object
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, 
+                                         request.FILES,
+                                         instance=request.user.profile)
+        if user_form.is_valid and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('user-profile')
+    else:
+        # Instance: is the instance of the expected object
+        # request contains the current user object
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form
+    }
+    return render(request, 'profile.html', context)
