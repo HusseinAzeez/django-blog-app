f70a271067bc0ace9697e1d6f48d5e572e81d44a
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-30 07:58:13 +0700

    Feat: Add create post view

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.urls import path
 from blog import views
-from blog.views import PostListView, PostDetailView
+from blog.views import (
+    PostListView, 
+    PostDetailView,
+    PostCreateView
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 # Local imports
 from blog.models import Post
 
@@ -12,6 +17,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     context = {
