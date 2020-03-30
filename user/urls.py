32bf3238f0f08e29a5f008c9b242ee6afb59df5d
HusseinAzeez <hussein.azeez91@gmail.com>
2020-03-30 12:27:01 +0700

    Feat: Add reset password

--- user/urls.py
+++ user/urls.py
@@ -15,12 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from django.contrib.auth import views
-from user import views as user_views
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView,
+    PasswordResetConfirmView
+)
+from user import views
 
 urlpatterns = [
-    path('profile/', user_views.profile, name='user-profile'),
-    path('login/', views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
-    path('logout/', views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
-    path('register/', user_views.register, name='user-register')
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    path('password-reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/done', PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset-done')
+
 ]
